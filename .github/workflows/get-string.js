b1a0793c5eace35095f3f801cd60370c5515b129
ardittristan <ardittristan@gmail.com>
2020-08-12 23:59:33 +0200

    should be final

--- .github/workflows/get-string.js
+++ .github/workflows/get-string.js
@@ -1,4 +1,4 @@
 var fs = require('fs')
 var object = fs.readFileSync('datacalc.js', 'utf-8')
 var entries = object.match(/(?<=^    "?)([^ "].*[^"])(?="?:)/gm)
-console.log(`{ exports: "${entries.toString()}" }`)
+console.log(`"{ exports: "${entries.toString()}" }"`)
